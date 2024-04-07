f727269e6db2c3f0ac1f5014ec3b2cdd15253b60
TzuChaeDahy <viniciuslovestwice@gmail.com>
2024-04-07 19:07:30 -0300

    create person class

---
+++ src/main/java/br/com/tzuchaedahy/restwithspringboot/person/model/Person.java
@@ -0,0 +1,45 @@
+package br.com.tzuchaedahy.restwithspringboot.person.model;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Person {
+    private long id;
+    private String firstName;
+    private String lastName;
+    private int Age;
+
+    public Person() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return Age;
+    }
+
+    public void setAge(int age) {
+        Age = age;
+    }
+}
