53cb5ed036403c175276db31a8a9d1f16d1c2daa
TzuChaeDahy <viniciuslovestwice@gmail.com>
2024-04-08 22:22:42 -0300

    implement code versioning

--- src/main/java/br/com/tzuchaedahy/restwithspringboot/person/controller/PersonController.java
+++ src/main/java/br/com/tzuchaedahy/restwithspringboot/person/controller/PersonController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/person")
+@RequestMapping(value = "/api/v1/person")
 public class PersonController {
 
     @Autowired
