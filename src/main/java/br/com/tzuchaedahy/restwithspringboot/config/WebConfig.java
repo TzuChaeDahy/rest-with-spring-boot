425602cead58add8c814aa59f8028c4a1f0c51f4
TzuChaeDahy <viniciuslovestwice@gmail.com>
2024-04-13 14:42:09 -0300

    implementing cors validation

--- src/main/java/br/com/tzuchaedahy/restwithspringboot/config/WebConfig.java
+++ src/main/java/br/com/tzuchaedahy/restwithspringboot/config/WebConfig.java
@@ -1,10 +1,12 @@
 package br.com.tzuchaedahy.restwithspringboot.config;
 
 import br.com.tzuchaedahy.restwithspringboot.util.YamlJackson2HttpMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -13,6 +15,20 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private static final MediaType APPLICATION_YAML = MediaType.valueOf("application/x-yaml");
+
+    @Value("${cors.originPatterns}")
+    private String corsOriginPatterns = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String[] allowedOrigins = corsOriginPatterns.split(",");
+
+        registry.
+                addMapping("/**").
+                allowedMethods("*").
+                allowedOrigins(allowedOrigins).
+                allowCredentials(true);
+    }
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
